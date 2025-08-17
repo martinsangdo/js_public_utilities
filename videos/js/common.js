a5ae792a8d19bc3a72884f2c4bfd507d74baa239
Martin Sang Do <dothanhsang1984@yahoo.com>
2025-08-17 12:09:56 +0700

    - init postion of bars

--- videos/js/common.js
+++ videos/js/common.js
@@ -2,3 +2,9 @@ function print(str){
     console.log(str);
 }
 
+function updateClock(divId) {
+    const now = new Date();
+    const minutes = String(now.getMinutes()).padStart(2, '0');
+    const seconds = String(now.getSeconds()).padStart(2, '0');
+    document.getElementById('divClock').textContent = `${minutes}:${seconds}`;
+}
